36988f8602f027186a33a0c06c9bdadb80638952
Sagarika Jaiswal <sagarikajaiswal97@gmail.com>
2024-08-07 16:57:56 +0530

    Feat: added joins for get all flights

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -38,7 +38,6 @@ async function getAllFlights(query){
         const [departureAirportId, arrivalAirportId] = query.trips.split("-");
         customFilter.departureAirportId = departureAirportId;
         customFilter.arrivalAirportId = arrivalAirportId;
-        console.log(departureAirportId, arrivalAirportId)
     }
     // price: 3000-6000
     if(query.price){
@@ -67,6 +66,7 @@ async function getAllFlights(query){
         const flights = await flightRepository.getAllFlights(customFilter, sortFilter);
         return flights;
     } catch (error) {
+        console.log(error);
         throw new AppError("Cannot fetch data of all flights", StatusCodes.INTERNAL_SERVER_ERROR);
     }
 }
